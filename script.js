9cd228c86de7fadeed1cd1eb56852ade59504738
Abdul Qoyyuum <abdul.qoyyuum@gmail.com>
2025-02-13 21:36:25 +0800

    :wrench: Form validation and submission logic added

--- script.js
+++ script.js
@@ -24,5 +24,15 @@ if (contactForm) {
     contactForm.addEventListener('submit', (e) => {
         e.preventDefault();
         // Add form validation and submission logic here
+        const myForm = e.target;
+        const formData = new FormData(myForm);
+
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString()
+        })
+            .then(() => console.log("Form successfully submitted"))
+            .catch(error => alert(error));
     });
 }
